24c5b1d21fa1a4db9c83d75910f897e31b2f44d9
Winnie <wimacoj@gmail.com>
2023-09-29 19:13:20 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function divide(){
+    let result = 2000 / 100;
+    return result;
+}
+
+divide();
+
+//arrow function
+
+const square = (num) => num * num;
+square(5);
+
+//arrow function two
+
+const add = (num1, num2) => num1 + num2;
+add(12,5);
